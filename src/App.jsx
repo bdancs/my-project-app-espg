04d538eb2b7f8eefcd5bb6b7a23809d820960a5e
bdancs <rm551645@fiap.com.br>
2023-08-22 11:59:50 -0300

    Cabecalho add(o meu não funciona)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+//rfc + enter
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+
+        <Cabecalho/>
+
+        <section>
+          {/*Isso é um comentário*/}
+          <p><a href="/">Home Page</a></p>
+
+            <button onClick={() => alert("Hello World!")}>Click Me!</button>
+        </section>
+
+        <footer>
+          <p>&copy; 2021, Vite.js and React</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
---
+++ src/componentes/Cabecalho.jsx
@@ -0,0 +1,9 @@
+export default function Cabecalho() {
+  return (
+    <>
+        <header>
+          <h1>Vite + React</h1>
+        </header>
+    </>
+  )
+}
