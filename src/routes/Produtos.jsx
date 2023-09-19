8a131cd5baf8a0ac7e66a8d4b67ceddbc3ff30f9
bdancs <rm551645@fiap.com.br>
2023-09-19 11:30:56 -0300

    Tabela Produtos construída

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,47 +1,78 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ListaProdutosExterna } from "../components/ListaProdutosExterna";
 
 export default function Produtos() {
 
-  //Estrutura de declaração do useState.
-  const[counter,setCounter] = useState(0);
-
   document.title = "Lista de Produtos";
 
-  //Estrutura de declaração do useEffect que sempre executa.
-  useEffect(()=>{
-    console.log("Este useEffect renderiza sempre que ocorrer uma atualização neste componente ou em um elemento filho.");
-  });
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
 
-  //Estrutura de declaração do useEffect que executa uma única vez.
+  //Estrutura que recebe a lista de produtos externa e repassa para uma lista local.
   useEffect(()=>{
-    console.log("Este useEffect renderiza apenas uma vez no carregamento do componente.");
+    setListaProdutosLocal(ListaProdutosExterna);
   },[]);
-
-  //Estrutura de declaração do useState.
-  const[counter2,setCounter2] = useState(0);
-
-  //Estrutura de declaração do useEffect que executa sempre baseado em um determinado elemento. Este elemento pode der:
-  //Uma constante, um componente, um objeto e ou uma variável. Que deve ser monitorados no arrayn de dependências. [ ]
-  useEffect(()=>{
-    console.log("Este useEffect renderiza apenas quando o objeto monitorado sofre atualização.");
-  },[counter2]);
-
-  // const handUseState = ()=>{
-  //   setCounter(1)
-  // }
 
   return (
     <div>
       <h1>Lista de Produtos</h1>
 
       <div>
-        <button onClick={()=>setCounter(counter + 1)}>COUNTER - {counter}</button>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>DESCRIÇÃO</th>
+              <th>PREÇO</th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={4}>PRODUTOS INFORMÁTICOS - QTD = </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
 
-      <div>
-        <button onClick={()=>setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-      </div>
     </div>
   )
 }
+
+      // <div>
+      //   <button onClick={()=>setCounter(counter + 1)}>COUNTER - {counter}</button>
+      // </div>
+
+      // <div>
+      //   <button onClick={()=>setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+      // </div>
+
+  // //Estrutura de declaração do useState.
+  // const[counter,setCounter] = useState(0);
+
+  // //Estrutura de declaração do useEffect que sempre executa.
+  // useEffect(()=>{
+  //   console.log("Este useEffect renderiza sempre que ocorrer uma atualização neste componente ou em um elemento filho.");
+  // });
+
+  // //Estrutura de declaração do useEffect que executa uma única vez.
+  // useEffect(()=>{
+  //   console.log("Este useEffect renderiza apenas uma vez no carregamento do componente.");
+  // },[]);
+
+  // //Estrutura de declaração do useState.
+  // const[counter2,setCounter2] = useState(0);
+
+  // //Estrutura de declaração do useEffect que executa sempre baseado em um determinado elemento. Este elemento pode der:
+  // //Uma constante, um componente, um objeto e ou uma variável. Que deve ser monitorados no arrayn de dependências. [ ]
+  // useEffect(()=>{
+  //   console.log("Este useEffect renderiza apenas quando o objeto monitorado sofre atualização.");
+  // },[counter2]);
+
+  // const handUseState = ()=>{
+  //   setCounter(1)
+  // }
