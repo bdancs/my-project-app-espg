e165e7e6c31f25eb48b7dc620843ac3181156802
bdancs <rm551645@fiap.com.br>
2023-10-10 10:44:36 -0300

    Imagem adicionada ao Home, sass adicionado ao cabecalho

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      
+      <div>
+        <h2>Produtos em Oferta...</h2>
+
+        <figure>
+          <img src="/public/img/supermarket-shelf-1094812_640.png" alt="prateleira" />
+
+          <figcaption>Nossos produtos</figcaption>
+        </figure>
+      </div>
+    </div>
   )
 }
