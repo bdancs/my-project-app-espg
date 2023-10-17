904166fff767861b347cc48ce2b42cb2b20acf8c
bdancs <rm551645@fiap.com.br>
2023-10-17 11:13:10 -0300

    Editar produto-PUT finalizado

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function EditarProdutos() {
 
@@ -7,6 +7,8 @@ export default function EditarProdutos() {
   document.title = "EDITAR PRODUTOS";
 
   const {id} = useParams();
+
+  const navigate = useNavigate();
 
   const [produto, setProduto] = useState({
     id:id,
@@ -26,35 +28,63 @@ export default function EditarProdutos() {
 
   }, [id])
 
-//   const handleChange = (e)=>{
+  const handleChange = (e)=>{
 
-//     const {name,value} = e.target;
-//   setProduto({[name]:value});
+    //Destructuring
+    const {name,value} = e.target;
 
-// }
+    //Preencher o useState com a função set... Utilizando o operador SPREAD...
+    setProduto({...produto,[name]:value});
+
+  }
+
+  //Função para fazer a alterações dos dados na base de dados
+  const handleSubmit = (e)=>{
+
+    //O 'preventDefault' previne de recarregar
+    e.preventDefault();
+
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: "PUT",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      //No body vai o produto porque é o que a gente quer editar
+      //O "JSON.stringify" é para transformar em texto
+      body: JSON.stringify(produto)
+
+    })
+
+    .then(response => console.log("STATUS DA REQUISIÃO: " + response.status))
+    .catch(error => console.log(error))
+
+    //Redirect
+    navigate("/produtos");
+
+  }
 
   return (
     <div>
         <h1>Editar Produtos</h1>
 
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <fieldset>
               <legend>Produto Selecionado</legend>
 
               <div>
                 <label htmlFor="idNome">Nome</label>
-                <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto" value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+                <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto" value={produto.nome} onChange={handleChange}/>
               </div>
 
               <div>
                 <label htmlFor="idDesc">Descrição</label>
-                <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto" value={produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+                <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto" value={produto.desc} onChange={handleChange}/>
               </div>
 
               <div>
                 <label htmlFor="idPreco">Preço</label>
-                <input type="number" name="preco" id="idPreco" placeholder="Digite o preço do produto" value={produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+                <input type="number" name="preco" id="idPreco" placeholder="Digite o preço do produto" value={produto.preco} onChange={handleChange}/>
               </div>
 
               <div>
